b2542709993ba8d2002c04a706c28a6ca8b68ba2
dpc761218914 <761218914@qq.com>
2018-07-28 17:04:09 +0800

    add introduce

--- app/router.js
+++ app/router.js
@@ -5,7 +5,10 @@
  */
 module.exports = app => {
   const { router, controller } = app;
+  //测试传入整型参数的方法
   router.get('/', controller.home.index);
+  //测试map为空的方法
   router.get('/classInfo', controller.classinfo.test);
+  //测试map为四个参数的方法
   router.get('/userTeacher', controller.userteacher.test);
 };
